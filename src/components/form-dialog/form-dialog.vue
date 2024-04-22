d6c734f449a559445f221d40daf542baef1cc84e
panboren <384953161@qq.com>
2024-04-22 18:01:12 +0800

    添加动画

--- src/components/form-dialog/form-dialog.vue
+++ src/components/form-dialog/form-dialog.vue
@@ -6,60 +6,125 @@
 -->
 <template>
   <el-dialog
-      :append-to-body="true"
-      :draggable="true"
-      :model-value="visibel"
-      @update:modelValue="input"
-      title="编辑"
-      width="500"
-      @close="onclose"
+    :modal="false"
+    :append-to-body="true"
+    :draggable="true"
+    :model-value="visibel"
+    @update:modelValue="input"
+    title="编辑"
+    width="580"
+    @close="onclose"
   >
-    <el-form ref="form" :model="formData" label-width="80px">
+    <el-form ref="form" :model="formData" label-width="80px" :inline="true">
       <el-form-item label="选择类型">
         <el-select v-model="formData.type" placeholder="请选择类型">
           <el-option
-              v-for="item in typeOption"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
+            v-for="item in typeOption"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
           />
         </el-select>
       </el-form-item>
-      <el-form-item label="添加文字" v-if="formData.type === 'text'">
+      <el-form-item label="宽">
+        <el-input-number v-model="formData.width" :min="1" />
+      </el-form-item>
+      <el-form-item label="高">
+        <el-input-number v-model="formData.height" :min="1" />
+      </el-form-item>
+      <el-form-item label="圆角">
+        <el-input-number v-model="formData.height" :min="1" />
+      </el-form-item>
+      <el-form-item label="阴影">
+        <el-input-number v-model="formData.height" :min="1" />
+      </el-form-item>
+      <el-form-item label="阴影颜色">
+        <el-color-picker v-model="formData.height" show-alpha />
+      </el-form-item>
+      <el-form-item label="背景颜色">
+        <el-color-picker v-model="formData.height" show-alpha />
+      </el-form-item>
+      <el-form-item label="添加文字" style="width: 100%" v-if="formData.type === 'text'">
         <el-input v-model="formData.content" />
       </el-form-item>
-      <el-form-item label="添加图片" v-if="formData.type === 'img'">
-        <div  class="form-img-main" v-show="formData.imgUrl">
+      <el-form-item label="文字尺寸" v-if="formData.type === 'text'">
+        <el-input-number v-model="formData.height" :min="1" />
+      </el-form-item>
+      <el-form-item label="文字加粗" v-if="formData.type === 'text'">
+        <el-input-number v-model="formData.height" :min="1" />
+      </el-form-item>
+      <el-form-item label="文字颜色" v-if="formData.type === 'text'">
+        <el-color-picker v-model="formData.height" />
+      </el-form-item>
+
+      <el-form-item label="添加图片" v-if="formData.type === 'img'" style="width: 100%">
+        <div class="form-img-main" v-show="formData.imgUrl">
           <img :src="formData.imgUrl" class="form-img" />
-          <el-icon @click.stop="remove"  class="form-img-close"><Close /></el-icon>
+          <el-icon @click.stop="remove" class="form-img-close"><Close /></el-icon>
         </div>
         <el-upload
-            v-show="!formData.imgUrl"
-            ref="upload"
-            class="avatar-uploader"
-            :show-file-list="false"
-            :on-change="change"
-            :auto-upload="false"
-            :limit="1"
+          v-show="!formData.imgUrl"
+          ref="upload"
+          class="avatar-uploader"
+          :show-file-list="false"
+          :on-change="change"
+          :auto-upload="false"
+          :limit="1"
         >
-          <div  class="form-img-main">
-            <el-icon  class="avatar-uploader-icon"><Plus /></el-icon>
+          <div class="form-img-main">
+            <el-icon class="avatar-uploader-icon"><Plus /></el-icon>
           </div>
         </el-upload>
       </el-form-item>
+      <el-form-item label="添加动画" style="width: 100%">
+        <el-button type="primary" :icon="Plus" />
+      </el-form-item>
+      <el-form-item label="过度时间">
+        <el-input-number v-model="formData.height" :min="1" />
+      </el-form-item>
+      <el-form-item label="选择类型">
+        <el-select v-model="formData.type" placeholder="请选择类型">
+          <el-option
+            v-for="item in typeOption"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </el-form-item>
+
+      <!--      <el-form-item label="延迟时间">
+        <el-input-number v-model="formData.height" :min="1" />
+      </el-form-item>
+      <el-form-item label="重复次数">
+        <el-input-number v-model="formData.height" :min="1" />
+      </el-form-item>
+
+      <el-form-item label="动画方向">
+        <el-input-number v-model="formData.height" :min="1" />
+      </el-form-item>-->
+
+      <!--      <el-form-item label="选择类型">
+        <el-select v-model="formData.type" placeholder="请选择类型">
+          <el-option
+            v-for="item in typeOption"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </el-form-item>-->
     </el-form>
     <template #footer>
       <div class="dialog-footer">
         <el-button @click="onclose">取消</el-button>
-        <el-button type="primary" @click="saveData">
-          保存
-        </el-button>
+        <el-button type="primary" @click="saveData"> 保存 </el-button>
       </div>
     </template>
   </el-dialog>
 </template>
 <script setup lang="ts">
-import { Plus , Close} from '@element-plus/icons-vue'
+import { Plus, Close } from '@element-plus/icons-vue'
 
 const props = defineProps({
   visibel: {
@@ -78,9 +143,7 @@ let emit = defineEmits(['update:visibel', 'update:data'])
 
 const upload = ref(null)
 
-
-
-let formData = ref({...props.data})
+let formData = ref({ ...props.data })
 let typeOption = ref([
   {
     value: 'img',
@@ -89,6 +152,10 @@ let typeOption = ref([
   {
     value: 'text',
     label: '文本'
+  },
+  {
+    value: 'div',
+    label: '空框'
   }
   // {
   //   value: 'audio',
@@ -100,38 +167,35 @@ let typeOption = ref([
   // }
 ])
 
-
 const handleExceed = () => {
   upload.value!.clearFiles()
 }
 
-const remove = ()=>{
+const remove = () => {
   formData.value.imgUrl = ''
   handleExceed()
 }
 
-
-
-const change = (file)=>{
+const change = (file) => {
   let url = URL.createObjectURL(file.raw!)
   formData.value.imgUrl = url
 }
 
-const input = (value)=>{
+const input = (value) => {
   emit('update:visibel', value)
 }
 // 关闭弹窗
-const onclose = ()=>{
+const onclose = () => {
   emit('update:visibel', false)
 }
 
-const saveData = ()=>{
+const saveData = () => {
   emit('update:data', formData.value)
   onclose()
 }
 </script>
 <style scoped>
-.form-img-main{
+.form-img-main {
   position: relative;
   width: 100px;
   height: 100px;
@@ -140,16 +204,15 @@ const saveData = ()=>{
   justify-content: center;
   border-radius: 4px;
   border: 1px solid #dcdfe6;
-  .form-img{
+  .form-img {
     width: 100px;
     height: 100px;
     display: block;
   }
-  .form-img-close{
+  .form-img-close {
     position: absolute;
     right: 5px;
     top: 5px;
   }
 }
-
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="content" id="contentRef" @dblclick="add">
     <el-button @click="show" class="show">预览</el-button>
-    <vue-draggable-resizable  v-for="(item,index) in listData" :key="item.id+index"
-                              :x="item.x"  :y="item.y"  :w="item.width" :h="item.height"
-                              @dragStop="onDragstop" @resizeStop="onResizestop"
-                              @activated="onActivated(item)"  @deactivated="onDeactivated"
-                              :parent="true" >
-        <div class="list-item"  :class="[item.className]">
-          <el-icon class="list-item-edit" @click.stop="edit(item)"><Edit /></el-icon>
-          <img  v-if="item.formData.type==='img'" :src="item.formData.imgUrl" class="list-item-img" />
-          <div v-if="item.formData.type==='text'" class="list-item-img">{{item.formData.content}}</div>
-          <form-dialog v-if="item.dialogVisible" v-model:visibel="item.dialogVisible" v-model:data="item.formData"/>
+    <vue-draggable-resizable
+      class-name="my-class-draggable-resizable"
+      :draggable="isShow"
+      :resizable="isShow"
+      :disableUserSelect="isShow"
+      v-for="(item, index) in listData"
+      :key="item.id + index"
+      :x="item.x"
+      :y="item.y"
+      :w="item.width"
+      :h="item.height"
+      @dragStop="onDragstop"
+      @resizeStop="onResizestop"
+      @activated="onActivated(item)"
+      @deactivated="onDeactivated"
+      :parent="true"
+    >
+      <div class="list-item-main" :class="[item.className]">
+        <div class="list-item">
+          <el-icon class="list-item-edit" @click.stop="onEdit(item)"><Edit /></el-icon>
+          <img
+            v-if="item.formData.type === 'img'"
+            :src="item.formData.imgUrl"
+            class="list-item-img"
+          />
+          <div v-if="item.formData.type === 'text'" class="list-item-img">
+            {{ item.formData.content }}
+          </div>
+          <form-dialog
+            v-if="item.dialogVisible"
+            v-model:visibel="item.dialogVisible"
+            v-model:data="item.formData"
+          />
         </div>
+      </div>
     </vue-draggable-resizable>
   </div>
 </template>
@@ -21,22 +45,23 @@ import { Edit } from '@element-plus/icons-vue'
 import VueDraggableResizable from 'vue-draggable-resizable'
 
 import gsap from 'gsap'
-import {ScrollTrigger} from 'gsap/ScrollTrigger.js'
+import { ScrollTrigger } from 'gsap/ScrollTrigger.js'
 gsap.registerPlugin(ScrollTrigger)
-
-
 
 let listData = ref([]) // 数据
 let activeItem = ref(null) // 激活
 let editItem = ref({}) // 编辑数据
 
-const show = ()=>{
+let isShow = ref(true)
+
+const show = () => {
+  console.log(listData)
+  isShow.value = !isShow.value
   run()
 }
 
-
 // 编辑
-const edit = (item)=>{
+const onEdit = (item) => {
   editItem.value = item
   editItem.value.dialogVisible = true
   console.log('edit')
@@ -47,53 +72,70 @@ const edit = (item)=>{
 // }
 
 // 创建
-const add = ($event)=>{
+const add = ($event) => {
   console.log($event)
   listData.value.push({
     name: 'panboren' + listData.value.length,
-    id:listData.value.length,
-    x:$event.offsetX,
-    y:$event.offsetY,
-    width:200,
-    height:200,
+    id: listData.value.length,
+    x: $event.offsetX,
+    y: $event.offsetY,
+    width: 200,
+    height: 200,
     dialogVisible: false,
     className: 'box' + listData.value.length,
-    formData:{
+    formData: {
       type: 'img',
       content: '',
       imgUrl: ''
+    },
+    amimation: {
+      x: 300,
+      rotation: 360,
+      duration: 3
+      // left: $event.offsetX,
+      // top: $event.offsetY
+      // scaleX: 1,
+      // scaleY: 1,
+      // rotation: 90,
+      // skewX: 0,
+      // skewY: 0,
+      // opacity: 1,
+      // repeat: -1,
+      // delay: 0,
+      // yoyo: true,
+      // duration: 1,
+      // ease: "power1.out",
     }
   })
 }
 
-
-const onActivated = (item)=>{
+const onActivated = (item) => {
   activeItem.value = item
-  console.log('onActivated',item)
+  console.log('onActivated', item)
 }
 
-const onDeactivated =  (item)=>{
+const onDeactivated = (item) => {
   activeItem.value = null
-  console.log('onDeactivated',item)
+  console.log('onDeactivated', item)
 }
 
-const onDragstop = (x,y)=>{
+const onDragstop = (x, y) => {
   // left the X
   // top the Y
-  console.log('dragstop',x,y)
-  if(activeItem.value) {
-    listData.value.forEach((item)=>{
-      if(item.id === activeItem.value.id) {
+  console.log('dragstop', x, y)
+  if (activeItem.value) {
+    listData.value.forEach((item) => {
+      if (item.id === activeItem.value.id) {
         item.x = x
         item.y = y
       }
     })
   }
 }
-const onResizestop = (x,y,width,height)=>{
-  if(activeItem.value) {
-    listData.value.forEach((item)=>{
-      if(item.id === activeItem.value.id) {
+const onResizestop = (x, y, width, height) => {
+  if (activeItem.value) {
+    listData.value.forEach((item) => {
+      if (item.id === activeItem.value.id) {
         item.x = x
         item.y = y
         item.width = width
@@ -101,78 +143,324 @@ const onResizestop = (x,y,width,height)=>{
       }
     })
   }
-  console.log('resizestop',x,y,width,height)
+  console.log('resizestop', x, y, width, height)
 }
 
-const run = ()=>{
-  ScrollTrigger.create({
-    trigger: '#contentRef',
-    start: 'top top',
-    // endTrigger: '#otherID',
-    end: 'bottom 50%+=100px',
-    onToggle: (self) => console.log('toggled, isActive:', self.isActive),
-    onUpdate: (self) => {
-      console.log(self)
-    }
-  })
+const run = () => {
+  // ScrollTrigger.create({
+  //   trigger: '#contentRef',
+  //   start: 'top top',
+  //   // endTrigger: '#otherID',
+  //   end: 'bottom 50%+=100px'
+  //   // onToggle: (self) => console.log('toggled, isActive:', self.isActive),
+  //   // onUpdate: (self) => {
+  //   //   console.log(self)
+  //   // }
+  // })
 
-  if(listData.value && listData.value.length > 1) {
-
-    gsap.to('.box0', {
-      scrollTrigger: '.box0', // start animation when ".box" enters the viewport
-      x: 1000,
-      y: 50
+  if (listData.value && listData.value.length > 0) {
+    listData.value.forEach((item) => {
+      console.log(item.className, item.amimation)
+      gsap.to(`.${item.className}`, {
+        ...item.amimation,
+        scrollTrigger: {
+          trigger: `.${item.className}`,
+          start: 'top center',
+          end: 'bottom center',
+          scrub: true
+        }
+      })
     })
+
+    /* gsap.to('.box0', {
+      rotation: 360,
+      duration: 3,
+      onUpdate: (self) => console.log('direction:', self),
+      scrollTrigger: {
+        trigger: '.box0',
+        start: 'top center',
+        end: 'bottom center',
+        scrub: true
+      }
+    })
+
     gsap.to('.box1', {
-      scrollTrigger: '.box1', // start animation when ".box" enters the viewport
-      rotate: 360
-    })
-
+      rotation: 360,
+      duration: 3,
+      onUpdate: (self) => console.log('direction:', self),
+      scrollTrigger: {
+        trigger: '.box1',
+        start: 'top center',
+        end: 'bottom center',
+        scrub: true
+      }
+    })*/
   }
 }
-
-
-
 </script>
 <style>
-@import "vue-draggable-resizable/style.css";
+@import 'vue-draggable-resizable/style.css';
 </style>
 <style scoped>
-
-.content{
+.content {
   position: relative;
   width: 100%;
-  height: 2000px;
+  height: 3000px;
 }
-.show{
+.show {
   position: absolute;
   left: 20px;
   top: 20px;
 }
-.list-item{
+.list-item-main {
+  width: 100%;
+  height: 100%;
+  display: block;
+  .list-item {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: block;
+    .list-item-edit {
+      position: absolute;
+      right: 5px;
+      top: 5px;
+    }
+    .list-item-img {
+      width: 100%;
+      height: 100%;
+      display: block;
+    }
+  }
+}
+.my-class-draggable-resizable {
+  position: absolute;
+}
+</style>
+
+<!--<template>
+  <div class="content" id="contentRef" @dblclick="add">
+    <el-button @click="show" class="show">预览</el-button>
+    <vue-draggable-resizable
+      v-show="!isShow"
+      v-for="(item, index) in listData"
+      :key="item.id + index"
+      :x="item.x"
+      :y="item.y"
+      :w="item.width"
+      :h="item.height"
+      @dragStop="onDragstop"
+      @resizeStop="onResizestop"
+      @activated="onActivated(item)"
+      @deactivated="onDeactivated"
+      :parent="true"
+    >
+      <div class="list-item">
+        <el-icon class="list-item-edit" @click.stop="edit(item)"><Edit /></el-icon>
+        <img
+          v-if="item.formData.type === 'img'"
+          :src="item.formData.imgUrl"
+          class="list-item-img"
+        />
+        <div v-if="item.formData.type === 'text'" class="list-item-img">
+          {{ item.formData.content }}
+        </div>
+        <form-dialog
+          v-if="item.dialogVisible"
+          v-model:visibel="item.dialogVisible"
+          v-model:data="item.formData"
+        />
+      </div>
+    </vue-draggable-resizable>
+    <div
+      v-show="isShow"
+      v-for="(item, index) in listData"
+      :key="item.id + index"
+      :style="{
+        transform: 'translate(' + item.x + 'px,' + item.y + 'px)',
+        width: item.width + 'px',
+        height: item.height + 'px'
+      }"
+      :class="[item.className]"
+    >
+      <div class="list-item">
+        <el-icon class="list-item-edit" @click.stop="edit(item)"><Edit /></el-icon>
+        <img
+          v-if="item.formData.type === 'img'"
+          :src="item.formData.imgUrl"
+          class="list-item-img"
+        />
+        <div v-if="item.formData.type === 'text'" class="list-item-img">
+          {{ item.formData.content }}
+        </div>
+        <form-dialog
+          v-if="item.dialogVisible"
+          v-model:visibel="item.dialogVisible"
+          v-model:data="item.formData"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Edit } from '@element-plus/icons-vue'
+import VueDraggableResizable from 'vue-draggable-resizable'
+
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger.js'
+gsap.registerPlugin(ScrollTrigger)
+
+let listData = ref([]) // 数据
+let activeItem = ref(null) // 激活
+let editItem = ref({}) // 编辑数据
+
+let isShow = ref(false)
+
+const show = () => {
+  console.log(listData)
+  isShow.value = !isShow.value
+  run()
+}
+
+// 编辑
+const edit = (item) => {
+  editItem.value = item
+  editItem.value.dialogVisible = true
+  console.log('edit')
+}
+// // 关闭弹窗
+// const onclose = ()=>{
+//   editItem.value = {}
+// }
+
+// 创建
+const add = ($event) => {
+  console.log($event)
+  listData.value.push({
+    name: 'panboren' + listData.value.length,
+    id: listData.value.length,
+    x: $event.offsetX,
+    y: $event.offsetY,
+    width: 200,
+    height: 200,
+    dialogVisible: false,
+    className: 'box' + listData.value.length,
+    formData: {
+      type: 'img',
+      content: '',
+      imgUrl: ''
+    }
+  })
+}
+
+const onActivated = (item) => {
+  activeItem.value = item
+  console.log('onActivated', item)
+}
+
+const onDeactivated = (item) => {
+  activeItem.value = null
+  console.log('onDeactivated', item)
+}
+
+const onDragstop = (x, y) => {
+  // left the X
+  // top the Y
+  console.log('dragstop', x, y)
+  if (activeItem.value) {
+    listData.value.forEach((item) => {
+      if (item.id === activeItem.value.id) {
+        item.x = x
+        item.y = y
+      }
+    })
+  }
+}
+const onResizestop = (x, y, width, height) => {
+  if (activeItem.value) {
+    listData.value.forEach((item) => {
+      if (item.id === activeItem.value.id) {
+        item.x = x
+        item.y = y
+        item.width = width
+        item.height = height
+      }
+    })
+  }
+  console.log('resizestop', x, y, width, height)
+}
+
+const run = () => {
+  // ScrollTrigger.create({
+  //   trigger: '#contentRef',
+  //   start: 'top top',
+  //   // endTrigger: '#otherID',
+  //   end: 'bottom 50%+=100px'
+  //   // onToggle: (self) => console.log('toggled, isActive:', self.isActive),
+  //   // onUpdate: (self) => {
+  //   //   console.log(self)
+  //   // }
+  // })
+
+  if (listData.value && listData.value.length > 1) {
+    gsap.to('.box0', {
+      rotation: 360,
+      duration: 3,
+      onUpdate: (self) => console.log('direction:', self),
+      scrollTrigger: {
+        trigger: '.box0',
+        start: 'top center',
+        end: 'bottom center',
+        scrub: true
+      }
+    })
+
+    gsap.to('.box1', {
+      rotation: 360,
+      duration: 3,
+      onUpdate: (self) => console.log('direction:', self),
+      scrollTrigger: {
+        trigger: '.box1',
+        start: 'top center',
+        end: 'bottom center',
+        scrub: true
+      }
+    })
+  }
+}
+</script>
+<style>
+@import 'vue-draggable-resizable/style.css';
+</style>
+<style scoped>
+.content {
+  position: relative;
+  width: 100%;
+  height: 3000px;
+}
+.show {
+  position: absolute;
+  left: 20px;
+  top: 20px;
+}
+.list-item {
   position: relative;
   width: 100%;
   height: 100%;
   display: block;
-  .list-item-edit{
+  .list-item-edit {
     position: absolute;
     right: 5px;
     top: 5px;
   }
-  .list-item-img{
+  .list-item-img {
     width: 100%;
     height: 100%;
     display: block;
   }
 }
-</style>
-
-
-
-
-
-
-
+</style>-->
 
 <!--
 <template>
@@ -326,33 +614,50 @@ const add = ($evwnt)=>{
 
 -->
 
-
-
-
-
-
-
-
-
-
 <!--
 <template>
   <div class="content" id="contentRef">
     <h1>index页面19999</h1>
     <div class="box">box</div>
-     <div class="box2">555</div>
-
+    <div class="box2">555</div>
   </div>
 </template>
 
 <script setup>
 import gsap from 'gsap'
-import {ScrollTrigger} from 'gsap/ScrollTrigger.js'
+import { ScrollTrigger } from 'gsap/ScrollTrigger.js'
 gsap.registerPlugin(ScrollTrigger)
 
+onMounted(() => {
+  gsap.to('.box', {
+    x: 600,
+    y: 100,
+    rotation: 360,
+    duration: 3,
+    onUpdate: (self) => console.log('direction:', self),
+    scrollTrigger: {
+      trigger: '.box',
+      start: 'top top',
+      end: 'bottom center',
+      scrub: true
+    }
+  })
 
-onMounted(()=>{
-  ScrollTrigger.create({
+  gsap.to('.box2', {
+    x: 600,
+    y: 300,
+    rotation: 360,
+    duration: 3,
+    onUpdate: (self) => console.log('direction:', self),
+    scrollTrigger: {
+      trigger: '.box2',
+      start: 'top top',
+      end: 'bottom center',
+      scrub: true
+    }
+  })
+
+  /*  ScrollTrigger.create({
     trigger: '#contentRef',
     start: 'top top',
     // endTrigger: '#otherID',
@@ -361,32 +666,56 @@ onMounted(()=>{
     onUpdate: (self) => {
       console.log(self)
     }
-  })
+  })*/
 
-  gsap.to('.box', {
+  /* gsap.to('.box', {
     scrollTrigger: '.box', // start animation when ".box" enters the viewport
-    x: 500
+    x: 500,
+    pin: true, // 保持元素在视窗中的位置
+    scrub: true // 允许拖动滚动条预览动画
   })
   gsap.to('.box2', {
     scrollTrigger: '.box2', // start animation when ".box" enters the viewport
-    rotate: 360
-  })
+    rotate: 360,
+    pin: true, // 保持元素在视窗中的位置
+    scrub: true // 允许拖动滚动条预览动画
+  })*/
 })
+
+// let tl = ScrollTrigger.create({
+//   trigger: '.box1',
+//   start: 'top center',
+//   end: '+=500'
+// })
+// tl = gsap.to('.green', {
+//   x: 600,
+//   y: 100,
+//   rotation: 360,
+//   duration: 3,
+//   onUpdate: (self) => console.log('direction:', self),
+//   scrollTrigger: {
+//     trigger: '.green',
+//     start: 'top top',
+//     end: 'bottom center',
+//     scrub: true
+//   }
+// })
 </script>
 <style scoped lang="scss">
-.content{
+.content {
   width: 100%;
   height: 2000px;
 
-  .box{
+  .box {
     width: 100%;
-    height:1200px;
+    height: 1500px;
     background: red;
   }
-  .box2{
+  .box2 {
     width: 100%;
-    height: 800px;
+    height: 1800px;
     background: #1bbea0;
   }
 }
-</style>-->
+</style>
+-->
